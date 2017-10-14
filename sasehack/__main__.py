f30107f2de64443625b85ad1d7111621f3f6768c
Josh Bowden <josbow@gmail.com>
2017-10-14 07:34:11 -0500

    feat: allow for invoking any abitraty question

--- sasehack/__main__.py
+++ sasehack/__main__.py
@@ -20,33 +20,70 @@ class FollowupEventSchema(ma.Schema):
 class FollowupEvent:
     def __init__(self, name, data=None):
         self.name = name
-        self.data = data or {}
-
-
-class ResponseSchema(ma.Schema):
-    speech = fields.Str()
-    display_text = fields.Str(attribute='displayText')
-    followup_event = fields.Nested(FollowupEventSchema)
+        self.data = data
 
 
 class Response:
-    def __init__(self, text, followup_event=None):
+    def __init__(self, text=None, followup_event=None):
         self.speech = text
         self.display_text = text
         self.followup_event = followup_event
 
 
+class ResponseSchema(ma.Schema):
+    speech = fields.Str()
+    displayText = fields.Str(attribute='display_text')
+    followupEvent = fields.Nested(FollowupEventSchema, attribute='followup_event')
+
+
+class UserInput:
+    def __init__(self, text: str, action: str, intent: str):
+        self.raw = text
+        self.action = action
+        self.intent = intent
+
+
+def respond_to(input: UserInput) -> Response:
+    if input.action == 'foobar':
+        return Response()
+
+    if input.action == 'input.unknown':
+        if not input.raw.startswith('/'):
+            return Response()
+
+        xs = input.raw.split(' ')
+        verb = xs[0].lstrip('/')
+        args = xs[1:]
+        print("verb={0!r}, args={1!r}".format(verb, args))
+
+        if verb == 'question':
+            if len(args) != 1:
+                return Response()
+
+            code = args[0]
+            return Response(followup_event=FollowupEvent('question-{}'.format(code)))
+
+        return Response()
+
+    if input.intent == 'start':
+        return Response(followup_event=FollowupEvent('initial_question'))
+
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
+    print(ResponseSchema().dump(Response(followup_event=FollowupEvent('question-{}'.format('a2')))).data)
+
     json = request.get_json()
     pprint(json)
 
+    raw = json['result']['resolvedQuery']
+    action = json['result']['action']
     intent = json['result']['metadata']['intentName']
-    print("INTENT='{}'".format(intent))
+    print("ACTION='{}', INTENT='{}'".format(action, intent))
 
     text = "THE WEBHOOK WORKS THIS TIME!!!"
-    result = Response(text, FollowupEvent('initial_question'))
-    pprint(result)
+    result = respond_to(UserInput(raw, action, intent))
+    print(ResponseSchema().dump(result).data)
 
     # {
     #     'speech': text,
