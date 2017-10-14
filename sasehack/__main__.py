8c7101e282c6adc56a83f2cd2b6ca5e9fb45f0cf
Josh Bowden <josbow@gmail.com>
2017-10-14 05:35:06 -0500

    feat: setup json schemas with working webhook

--- sasehack/__main__.py
+++ sasehack/__main__.py
@@ -1,10 +1,39 @@
+import collections
 import logging
 from pprint import pprint
 from flask import Flask
 from flask import request, jsonify
+from flask_marshmallow import Marshmallow
+from marshmallow import Schema, fields, pprint
 
 logging.basicConfig(level=logging.DEBUG)
+
 app = Flask(__name__)
+ma = Marshmallow(app)
+
+
+class FollowupEventSchema(ma.Schema):
+    name = fields.Str()
+    data = fields.Dict()
+
+
+class FollowupEvent:
+    def __init__(self, name, data=None):
+        self.name = name
+        self.data = data or {}
+
+
+class ResponseSchema(ma.Schema):
+    speech = fields.Str()
+    display_text = fields.Str(attribute='displayText')
+    followup_event = fields.Nested(FollowupEventSchema)
+
+
+class Response:
+    def __init__(self, text, followup_event=None):
+        self.speech = text
+        self.display_text = text
+        self.followup_event = followup_event
 
 
 @app.route('/webhook', methods=['POST'])
@@ -16,13 +45,19 @@ def webhook():
     print("INTENT='{}'".format(intent))
 
     text = "THE WEBHOOK WORKS THIS TIME!!!"
-    result = {
-        'speech': text,
-        'displayText': text
-    }
-
+    result = Response(text, FollowupEvent('initial_question'))
     pprint(result)
-    return jsonify(result)
+
+    # {
+    #     'speech': text,
+    #     'displayText': text,
+    #     'followupEvent': {
+    #         'name': 'initial_question',
+    #         "data": {}
+    #     }
+    # }
+
+    return ResponseSchema().jsonify(result)
 
 
 @app.route('/')
